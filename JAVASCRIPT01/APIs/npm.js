098d0235b765d1f3f50f67a854c52c5bdc8b0656
anaparastasi <137augusto@gmail.com>
2022-08-30 20:24:35 -0300

    atualizações dos estudos

---
+++ JAVASCRIPT01/APIs/npm.js
@@ -0,0 +1,7 @@
+/*Gerenciamento de Pacotes no JS.
+    Um gerenciador de pacotes(biblioteca de códigos prontos) serve para gerenciar pacotes e dependências do nosso projeto.
+
+    NPM(GITHUB)
+    Vem instalado junto ao Node.js
+
+*/
